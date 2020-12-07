e6ac77e22083c9cc54009386d83fd66001f71694
MotokiKasaharaBansuke <motoki.kasahara@plaid.co.jp>
2020-12-08 01:44:41 +0900

    一覧表示作成

--- src/features/mypage/bizpack/bizpackIndex.tsx
+++ src/features/mypage/bizpack/bizpackIndex.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect }  from 'react'
 import { useSelector } from "react-redux"
+import { Grid, CssBaseline, Paper } from '@material-ui/core';
 
 import { useAppDispatch } from "../../../../src/app/storeHelper";
 
@@ -7,10 +8,25 @@ import {
     selectBizpacks,
     fetchAsyncGetBizpacks
 } from "../mypageSlice"
+import BizpackCard from "./bizpackCard"
+import resGetBizpacksType from '../resGetBizpacks.json'
+
+type bizpack = typeof resGetBizpacksType.data[0]
 
 const BizPackIndex: React.FC = () => {
     const asyncDispatch = useAppDispatch();
     const bizpacks = useSelector(selectBizpacks)
+    const reshapeBizpack = (bizpack: bizpack) => {
+        return {
+            producs: bizpack.products.map(p => {return {name: p.name}}),
+            industry: bizpack.industry,
+            scale: bizpack.scale,
+            description: bizpack.description,
+            unitPrice: bizpack.unitPrice,
+            duration: bizpack.duration,
+            isPublic: bizpack.isPublic
+        }
+    }
 
     useEffect(()=>{
         asyncDispatch(fetchAsyncGetBizpacks())
@@ -20,8 +36,16 @@ const BizPackIndex: React.FC = () => {
 
     return (
         <div>
-            hoge
-            {JSON.stringify(bizpacks)}
+            <Grid container component="main" >
+            <CssBaseline />
+            <Grid item xs={12} sm={8} md={4} component={Paper} elevation={6} square>
+                {bizpacks.data.map(bizpack => {
+                    const rbp = reshapeBizpack(bizpack)
+                    return ( <BizpackCard props={rbp} /> )
+                })}
+            </Grid>
+             {JSON.stringify(bizpacks)}
+            </Grid>
         </div>
     )
 }
