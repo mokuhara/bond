ad102560042be5978a39d16b0fd86533152d645c
MotokiKasaharaBansuke <motoki.kasahara@plaid.co.jp>
2020-12-21 00:09:41 +0900

    review機能作成

--- src/pages/mypage/specialist/transaction/transaction.tsx
+++ src/pages/mypage/specialist/transaction/transaction.tsx
@@ -1,13 +1,18 @@
 import React, {useState} from 'react'
 import { useLocation } from 'react-router-dom'
-import {Grid, CssBaseline, FormControl, MenuItem, Select} from '@material-ui/core'
-import {makeStyles, Theme} from "@material-ui/core/styles";
+import {Grid, CssBaseline, FormControl, MenuItem, Select, TextField, Typography, Button} from '@material-ui/core'
+import {makeStyles, Theme, withStyles} from "@material-ui/core/styles";
+import Rating, { IconContainerProps } from '@material-ui/lab/Rating';
+import {SentimentVeryDissatisfied, SentimentDissatisfied, SentimentSatisfied, SentimentSatisfiedAlt, SentimentVerySatisfied} from "@material-ui/icons"
 
 import transactionJson from './transaction.json'
+import respReviewJson from './resReview.json'
 import fetcher from '../../../utils/fetcher'
 
 type transactionState = {transaction: typeof transactionJson}
 type transaction = typeof transactionJson
+type review = typeof transactionJson.reviews[0]
+type respReview = typeof respReviewJson
 
 const useStyles = makeStyles((theme: Theme) => ({
     label: {
@@ -24,21 +29,84 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
 }))
 
+const StyledRating = withStyles({
+    iconFilled: {
+      color: '#ff6d75',
+    },
+    iconHover: {
+      color: '#ff3d47',
+    },
+  })(Rating);
+
+const customIcons: { [index: string]: { icon: React.ReactElement; label: string } } = {
+    1: {
+      icon: <SentimentVeryDissatisfied />,
+      label: 'Very Dissatisfied',
+    },
+    2: {
+      icon: <SentimentDissatisfied />,
+      label: 'Dissatisfied',
+    },
+    3: {
+      icon: <SentimentSatisfied />,
+      label: 'Neutral',
+    },
+    4: {
+      icon: <SentimentSatisfiedAlt />,
+      label: 'Satisfied',
+    },
+    5: {
+      icon: <SentimentVerySatisfied />,
+      label: 'Very Satisfied',
+    },
+  };
+  
+  function IconContainer(props: IconContainerProps) {
+    const { value, ...other } = props;
+    return <span {...other}>{customIcons[value].icon}</span>;
+  }
+
 const TransactionIndex: React.FC = () => {
     const classes = useStyles()
     const location = useLocation<transactionState>();
-    const transaction = location.state.transaction
-    console.log(transaction)
+    const [transaction, setTransaction] = useState(location.state.transaction)
     const [status, setStatus] = useState(transaction.status)
+    const [review, setReview] = useState({rating:0, message:""})
 
-    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    const handleStatusChange = (event: React.ChangeEvent<{ value: unknown }>) => {
         const status = event.target.value
         if (typeof status === 'number'){
-            transaction.status = status
+            setTransaction({...transaction, status: status})
             setStatus(status)
             // asyncChangeStatus(transaction)
         }
     };
+
+    const handleReviewMessageChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        const message = event.target.value
+        if (typeof message === 'string') {
+            setReview({...review, message: message})
+        }
+    }
+
+    const handleReviewRatingChange = (event :React.ChangeEvent<{}>
+        , values: number | null): void => {
+        if( typeof values === 'number') {
+            setReview({...review, rating: values})
+        }
+    }
+
+    const handleSendReview = () => {
+        const payload = {
+            transactionId: transaction.id,
+            userId: transaction.bizpack.userId,
+            message: review.message,
+            rating: review.rating,
+        }
+
+        asyncCreateReview(payload)
+    }
+
 
     const apiUrl = "http://localhost:8000/v1";
 
@@ -51,6 +119,32 @@ const TransactionIndex: React.FC = () => {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(transaction)
+        })
+        return res
+    }
+
+    const asyncCreateReview = async (review: review) => {
+        const res = await fetcher<review>(`${apiUrl}/mypage/transaction/review/create`, {
+            mode: 'cors',
+            method: 'POST',
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(review)
+        })
+        return res
+    }
+
+    const asyncUpdateReview = async (review: respReview) => {
+        const res = await fetcher<review>(`${apiUrl}/mypage/transaction/review/${review.data.ID}/update`, {
+            mode: 'cors',
+            method: 'POST',
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(review)
         })
         return res
     }
@@ -84,7 +178,7 @@ const TransactionIndex: React.FC = () => {
                     <FormControl variant="outlined" className={classes.formControl}>
                         <Select
                         value={transaction.status}
-                        onChange={handleChange}
+                        onChange={handleStatusChange}
                         className={classes.selectEmpty}
                         >
                             <MenuItem selected={true} value={transaction.status}>aaaa</MenuItem>
@@ -94,6 +188,54 @@ const TransactionIndex: React.FC = () => {
                         </Select>
                     </FormControl>
                 </Grid>
+                {transaction.status === 4 && (
+                    <>
+                        <Grid item xs={12} className={classes.label}>
+                        <Typography component="legend">　レビュー</Typography>
+                     </Grid>
+                     <Grid item xs={6} className={classes.label}>
+                         評価
+                     </Grid>
+                     <Grid item xs={6} className={classes.data}>
+                         <Rating
+                             name="customized-icons"
+                             defaultValue={2}
+                             getLabelText={(value: number) => customIcons[value].label}
+                             IconContainerComponent={IconContainer}
+                             onChange={(event,value) =>handleReviewRatingChange(event, value)}
+                         />
+                     </Grid>
+                     <Grid item xs={6} className={classes.label}>
+                         内容
+                     </Grid>
+                     <Grid item xs={6} className={classes.data}>
+                         <TextField
+                             variant="outlined"
+                             margin="normal"
+                             fullWidth
+                             required
+                             id="description"
+                             label="description"
+                             name="description"
+                             autoComplete="description"
+                             autoFocus
+                             multiline
+                             rows={4}
+                             value={review.message}
+                             onChange={handleReviewMessageChange}
+                         />
+                     </Grid>
+                     <Button
+                         // fullWidth
+                         // type="submit"
+                         variant="contained"
+                         color="primary"
+                         onClick={handleSendReview}
+                     >
+                        送信する
+                     </Button>
+                    </>
+                )}
             </Grid>
         </>
     )
--- src/pages/mypage/specialist/transaction/transactionIndex.tsx
+++ src/pages/mypage/specialist/transaction/transactionIndex.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState }  from 'react'
-import { useSelector, useDispatch } from "react-redux"
-import { Paper,Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Chip, Button, Menu, MenuItem } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Paper,Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Button, Menu, MenuItem , Modal, TextField} from '@material-ui/core';
+import { makeStyles , Theme} from '@material-ui/core/styles';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
-import Transaction from './transaction'
 import transactionJson from './transaction.json'
 import fetcher from '../../../utils/fetcher'
+import respVideoMeetingJson from  './resVideoMeeting.json'
 
 interface Column {
     id: 'category' | 'title' | 'status' | 'description' | 'other';
@@ -25,17 +25,45 @@ interface Column {
     { id: 'other', label: 'その他', minWidth: 50}
   ];
 
+  function rand() {
+    return Math.round(Math.random() * 20) - 10;
+  }
+
+  function getModalStyle() {
+    const top = 50 + rand();
+    const left = 50 + rand();
+
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+
 const TransactionIndex: React.FC = () => {
     const demoData = [transactionJson, transactionJson, transactionJson]
     const history = useHistory()
-    const useStyles = makeStyles({
+    const useStyles = makeStyles((theme: Theme) => ({
         root: {
           width: '100%',
         },
         container: {
           maxHeight: 440,
         },
-      });
+        paper: {
+            position: 'absolute',
+            width: 400,
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+          },
+        textField: {
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+            width: 200,
+        },
+      }));
     const classes = useStyles();
     const initTableData = {
         id: 0,
@@ -45,19 +73,25 @@ const TransactionIndex: React.FC = () => {
         description: "description",
         transaction: transactionJson
     }
+    const { register, errors } = useForm({
+        mode: 'onBlur',
+        reValidateMode: 'onChange'
+      })
+    const btnDisabler = Boolean(errors.topic) || Boolean(errors.startAt)
     type tableData = typeof initTableData[]
     type transaction = typeof transactionJson
     type transactions = transaction[]
     type videoMeeting = typeof transactionJson.videoMeetings[0]
     type payment = typeof transactionJson.payments[0]
     type review = typeof transactionJson.reviews[0]
+    type respVideoMeeting = typeof respVideoMeetingJson
 
-    const apiUrl = "http://localhost:8000/v1";
+    const apiUrl = "http://localhost:3000/v1";
 
 
     const asyncGetTransactions = async () => {
         const res = await fetcher<transactions>(`${apiUrl}/mypage/transaction`, {
-            mode: 'no-cors',
+            mode: 'cors',
             method: 'GET',
             cache: "no-cache",
             headers: {
@@ -67,35 +101,9 @@ const TransactionIndex: React.FC = () => {
         return res
     }
 
-    const asyncCreateReview = async (revie: review) => {
-        const res = await fetcher<review>(`${apiUrl}/mypage/transaction/review/create`, {
-            mode: 'no-cors',
-            method: 'POST',
-            cache: "no-cache",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(revie)
-        })
-        return res
-    }
-
-    const asyncUpdateReview = async (review: review) => {
-        const res = await fetcher<review>(`${apiUrl}/mypage/transaction/review/${review.id}/update`, {
-            mode: 'no-cors',
-            method: 'POST',
-            cache: "no-cache",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(review)
-        })
-        return res
-    }
-
     const asyncCreatePayment = async (payment: payment) => {
         const res = await fetcher<payment>(`${apiUrl}/mypage/transaction/payment/create`, {
-            mode: 'no-cors',
+            mode: 'cors',
             method: 'POST',
             cache: "no-cache",
             headers: {
@@ -106,17 +114,29 @@ const TransactionIndex: React.FC = () => {
         return res
     }
 
-    const asyncCreateVideoMeeting = async (videoMeeting: videoMeeting) => {
-        const res = await fetcher<videoMeeting>(`${apiUrl}/mypage/transaction/videoMeeting/create`, {
-            mode: 'no-cors',
+    const asyncCreateVideoMeeting = async () => {
+        const body =  {
+            topic: videoMeeting.topic,
+            type: "2",
+            start_time: videoMeeting.start_time,
+            timezone: "Asia/Tokyo",
+            settings: {
+                use_pmi: "false"
+            }
+        }
+
+        const res = await fetcher<respVideoMeeting>(`${apiUrl}/mypage/videomeeting/create`, {
+            mode: 'cors',
             method: 'POST',
             cache: "no-cache",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(videoMeeting)
+            body: JSON.stringify(body)
         })
-        return res
+        if(res.data){
+            setVideoMeeting({ ...videoMeeting, join_url: res.data.join_url})
+        }
     }
 
     const [transactions, setTransactions] = useState([transactionJson])
@@ -126,7 +146,17 @@ const TransactionIndex: React.FC = () => {
     const [videoMeeting, setVideoMeeting] = useState(transactionJson.videoMeetings[0])
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = React.useState(false);
+    console.log(videoMeeting.join_url)
 
+    const handleOpenModal = () => {
+        setOpen(true);
+      };
+
+    const handleCloseModal = () => {
+    setOpen(false);
+    };
 
     const handleChangePage = (event: unknown, newPage: number) => {
         setPage(newPage);
@@ -161,26 +191,12 @@ const TransactionIndex: React.FC = () => {
         return result
     }
 
-    const dummy = () => {
-
-    }
-
     const moveTransaction = (transaction: transaction) => {
         history.push({
             pathname: '/mypage/transaction',
             state: {transaction}
         })
     }
-
-    // const moveEditBizpack = (bizpackId: number) => {
-    //   setAnchorEl(null);
-    //   dispatch(editId(bizpackId))
-    //   history.push("/mypage/bizpack/edit")
-    // };
-
-    // const DeleteBizpack = (bizpackId: number) => {
-    //   asyncDispatch(fetchAsyncDeleteBizpack(bizpackId))
-    // }
 
     // asyncGetTransactions().then(res=> setTransactions(res))
     useEffect(() => {
@@ -189,75 +205,138 @@ const TransactionIndex: React.FC = () => {
         setTableData(hoge)
     },[])
 
+    const dummy = () => {}
+
+    const body = (
+        <div style={modalStyle} className={classes.paper}>
+          <h2>web会議を作成する</h2>
+          <p>
+            タイトルと日付を入力してください
+          </p>
+          <form className={classes.container} noValidate>
+            <TextField
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                required
+                id="topic"
+                name="topic"
+                autoComplete="topic"
+                autoFocus
+                value={videoMeeting.topic}
+                onChange={(e)=>{setVideoMeeting({ ...videoMeeting, topic: e.target.value})}}
+                inputRef={register({ required: true })}
+                error={Boolean(errors.topic)}
+                helperText={errors.topic && "入力必須です"}
+            />
+            <TextField
+                id="startAt"
+                name="startAt"
+                type="datetime-local"
+                defaultValue="2017-05-24T10:30"
+                className={classes.textField}
+                InputLabelProps={{
+                shrink: true,
+                }}
+                onChange={(e)=>{setVideoMeeting({ ...videoMeeting, start_time: e.target.value})}}
+                inputRef={register({ required: true })}
+                error={Boolean(errors.startAt)}
+                helperText={errors.startAt && "入力必須です"}
+            />
+            <Button
+                    fullWidth
+                    // type="submit"
+                    variant="contained"
+                    color="primary"
+                    disabled={btnDisabler}
+                    onClick={()=>asyncCreateVideoMeeting()}
+                >
+                    bizpackを更新する
+            </Button>
+        </form>
+        {videoMeeting.join_url && (<a href={videoMeeting.join_url}>video会議はこちら</a>)}
+        </div>
+    );
+
     return (
-        <Paper className={classes.root}>
-          <TableContainer className={classes.container}>
-            <Table stickyHeader aria-label="sticky table">
-              <TableHead>
-                <TableRow>
-                  {columns.map((column) => (
-                    <TableCell
-                      key={column.id}
-                      align={column.align}
-                      style={{ minWidth: column.minWidth }}
-                    >
-                      {column.label}
-                    </TableCell>
-                  ))
-                  }
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {tableData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                  return (
-                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                      {columns.map((column) => {
-                        let value : string | number | JSX.Element[] | JSX.Element
-                        if(column.id === 'other'){
-                            return (
-                                <TableCell>
-                                    <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                                    <MoreVertIcon />
-                                    </Button>
-                                    <Menu
-                                    id="simple-menu"
-                                    anchorEl={anchorEl}
-                                    keepMounted
-                                    open={Boolean(anchorEl)}
-                                    onClose={handleClose}
-                                    >
-                                    <MenuItem onClick={() => moveTransaction(row.transaction)}>詳細</MenuItem>
-                                    {row["status"]===0 && <MenuItem onClick={() => dummy()}>受注</MenuItem>}
-                                    <MenuItem onClick={() => dummy()}>レビュー</MenuItem>
-                                    <MenuItem onClick={() => dummy()}>web会議作成</MenuItem>
-                                    </Menu>
-                                </TableCell>
-                            )
-                        } else {
-                            value = row[column.id]
-                            return (
-                            <TableCell key={column.id} align={column.align}>
-                                {value}
+        <>
+            <Paper className={classes.root}>
+                <TableContainer className={classes.container}>
+                    <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                        {columns.map((column) => (
+                            <TableCell
+                            key={column.id}
+                            align={column.align}
+                            style={{ minWidth: column.minWidth }}
+                            >
+                            {column.label}
                             </TableCell>
-                            );
+                        ))
                         }
-                      })}
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[10, 50,100]}
-            component="div"
-            count={tableData.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onChangePage={handleChangePage}
-            onChangeRowsPerPage={handleChangeRowsPerPage}
-          />
-        </Paper>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {tableData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                        return (
+                            <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                            {columns.map((column) => {
+                                let value : string | number | JSX.Element[] | JSX.Element
+                                if(column.id === 'other'){
+                                    return (
+                                        <TableCell>
+                                            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                                            <MoreVertIcon />
+                                            </Button>
+                                            <Menu
+                                            id="simple-menu"
+                                            anchorEl={anchorEl}
+                                            keepMounted
+                                            open={Boolean(anchorEl)}
+                                            onClose={handleClose}
+                                            >
+                                            <MenuItem onClick={() => moveTransaction(row.transaction)}>詳細</MenuItem>
+                                            {row["status"]===0 && <MenuItem onClick={() => dummy()}>受注</MenuItem>}
+                                            <MenuItem onClick={handleOpenModal}>web会議作成</MenuItem>
+                                            </Menu>
+                                        </TableCell>
+                                    )
+                                } else {
+                                    value = row[column.id]
+                                    return (
+                                    <TableCell key={column.id} align={column.align}>
+                                        {value}
+                                    </TableCell>
+                                    );
+                                }
+                            })}
+                            </TableRow>
+                        );
+                        })}
+                    </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[10, 50,100]}
+                    component="div"
+                    count={tableData.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+            </Paper>
+            <Modal
+                open={open}
+                onClose={handleCloseModal}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                {body}
+            </Modal>
+        </>
+        
       );
 }
 
