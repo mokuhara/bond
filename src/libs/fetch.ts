e5b118d73927547681d737f0f0dd444c6bf8619a
井上裕太郎 <inoue.devel@gmail.com>
2021-01-18 21:51:49 +0900

    envファイルの追加

--- src/libs/fetch.ts
+++ src/libs/fetch.ts
@@ -21,8 +21,7 @@ const defaultOptions = (isCredential: boolean) => {
 
   return options
 }
-
-export const apiUrl = "http://localhost:8000/v1";
+export const apiUrl = process.env.REACT_APP_API_URL
 
 export const get = (path: string, additionalOptions: object = {}, isCredential: boolean = true): Promise<Response> => {
   const options: object = Object.assign(defaultOptions(isCredential), additionalOptions)
