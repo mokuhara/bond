350ee92bf25af61341ac581407da5515e64687ac
MotokiKasaharaBansuke <motoki.kasahara@plaid.co.jp>
2021-01-21 22:41:32 +0900

    client-transaction-show修正

--- src/pages/mypage/client/transaction/show/index.tsx
+++ src/pages/mypage/client/transaction/show/index.tsx
@@ -1,7 +1,9 @@
 import React, {useState} from 'react'
 import { useLocation } from 'react-router-dom'
-import {Grid, CssBaseline, Card, CardContent, Typography } from '@material-ui/core'
+import {Grid, CssBaseline, Card, CardContent, Typography, Button } from '@material-ui/core'
 import {makeStyles, Theme } from "@material-ui/core/styles";
+import { useHistory } from 'react-router-dom';
+import { apiUrl, put } from '../../../../../libs/fetch'
 
 
 import CreateReview from '../review/new'
@@ -11,6 +13,7 @@ import { transactionState } from '../index/store'
 import Cookies from 'js-cookie'
 import VideoMeetings from '../videMeeting/index'
 import Chat from '../../../chat'
+
 
 type transaction = {transaction: typeof transactionState[0]}
 
@@ -39,9 +42,18 @@ const useStyles = makeStyles((theme: Theme) => ({
     selectEmpty: {
         marginTop: theme.spacing(2),
     },
+    buttonWrapper: {
+        margin: '2px',
+        padding: '2px'
+    },
+    button: {
+        fontSize: '12px',
+        margin: '10px'
+    }
 }))
 
 const Transaction: React.FC = () => {
+    const history = useHistory()
     const classes = useStyles()
     const location = useLocation<transaction>();
     const [transaction, setTransaction] = useState(location.state.transaction)
@@ -55,13 +67,59 @@ const Transaction: React.FC = () => {
         return false
     }
 
+    const moveEditIssue = () => {
+        history.push({
+            pathname: '/mypage/client/transaction/edit',
+            state: {transaction}
+        })
+
+    }
+
+    const toggleAccept = () => {
+        const clientUserId = Cookies.get('bd-uid')
+        if(typeof clientUserId != 'string') return
+        const newClientAcceptance = transaction.clientAcceptance === 1 ? 2 : 1
+        const body = {
+            id: transaction.ID,
+            description: transaction.description,
+            clientAcceptance: newClientAcceptance
+        }
+        setTransaction({...transaction, clientAcceptance: newClientAcceptance})
+        put(`${apiUrl}/mypage/transaction/${transaction.ID}/update`, body)
+    }
+
 
     return (
         <>
             <Grid container component="main" className={classes.root}>
                 <CssBaseline />
-                <Grid item xs={10}>
-                    <Typography variant="h6" component="h2" className={classes.title}>案件詳細</Typography>
+                <Grid item xs={10} >
+                    <Grid container justify="space-between">
+                        <Grid item xs={6}>
+                            <Typography variant="h6" component="h2" className={classes.title}>案件詳細</Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Grid container spacing={2} justify="center" alignItems="center" className={classes.buttonWrapper}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={()=>moveEditIssue()}
+                                className={classes.button}
+                                disabled={transaction.SpecialistAcceptance === 1 && transaction.clientAcceptance === 1}
+                            >
+                                編集する
+                            </Button>
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                onClick={()=>toggleAccept()}
+                                className={classes.button}
+                            >
+                                {transaction.clientAcceptance === 1 ? "承認を解除" : "承認する"}
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    </Grid>
                     <Grid item xs={12}>
                         <Card  className={classes.container} variant="outlined">
                             <CardContent>
@@ -70,19 +128,19 @@ const Transaction: React.FC = () => {
                                         タイトル
                                     </Grid>
                                     <Grid item xs={8} className={classes.data}>
-                                        {transaction.Bizpack.title}
+                                        {transaction.title}
                                     </Grid>
                                     <Grid item xs={4} className={classes.label}>
                                         カテゴリ
                                     </Grid>
                                     <Grid item xs={8} className={classes.data}>
-                                        {transaction.Bizpack.category.type}
+                                        {transaction.category.type}
                                     </Grid>
                                     <Grid item xs={4} className={classes.label}>
                                         詳細
                                     </Grid>
                                     <Grid item xs={8} className={classes.data}>
-                                        {transaction.Bizpack.description}
+                                        {transaction.description}
                                     </Grid>
                                     <Status transaction={transaction} setTransaction={setTransaction}/>
                                 </Grid>
