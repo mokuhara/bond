818526ab5622165c0e1dc6b5377aa06bbc1c94fb
MotokiKasaharaBansuke <motoki.kasahara@plaid.co.jp>
2021-01-22 19:38:51 +0900

    thread zoom room追加機能修正

--- src/pages/mypage/chat/thread/show/index.tsx
+++ src/pages/mypage/chat/thread/show/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { Grid , Card} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Layout from '../../../../../layouts/mypage'
 
 import MessageIndex from '../../components/messageBox'
 import MessageNew from '../../components/messageCreate'
@@ -12,24 +13,25 @@ const useStyles = makeStyles((theme) => ({
     },
     }))
 
-type threadId = { threadId : number }
+type payload = { threadId : number, transactionId: number}
 const ThreadShow: React.FC = () => {
     const classes = useStyles();
-    const location = useLocation<threadId>();
+    const location = useLocation<payload>();
     const threadId = location.state.threadId
+    const transactionId = location.state.transactionId
 
 
     return (
-        <>
+        <Layout>
             <Grid container justify="center">
                 <Grid item xs= {6}>
                     <Card variant="outlined" className={classes.root}>
                         <MessageIndex threadId={threadId} />
-                        <MessageNew threadId={threadId} />
+                        <MessageNew threadId={threadId} transactionId={transactionId}/>
                     </Card>
                 </Grid>
             </Grid>
-        </>
+        </Layout>
     )
 }
 
